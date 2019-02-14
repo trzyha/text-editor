ea45c7b3eb864e04f64ab5db6481ec089c2e4fc9
trzyha <39124933+trzyha@users.noreply.github.com>
2019-02-14 21:56:46 +0100

    Add files via upload

--- writer.py
+++ writer.py
@@ -1,13 +1,25 @@
 import tkinter as tk
+from tkinter import filedialog ##filedialog is tkinter dialog module needs to be imported seperetly
 
 def NewFile():
-	print("new file!!")
+	T.delete(1.0, tk.END)
 
 def OpenFile():
 	print("open file!")
+	root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
+	f = open(root.filename) ##opening file choosen from dialog
+	T.insert(1.0, f.read()) ##insert to text widget choosen file
 
 def About():
 	print("simple text editor")
+
+def Save():
+	print("save")
+
+def SaveAs():
+	root.filename =  filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
+	f = open(root.filename)
+	
 
 root = tk.Tk()
 
@@ -18,16 +30,18 @@ filemenu = tk.Menu(menu)
 menu.add_cascade(label = "File", menu=filemenu)
 filemenu.add_command(label="New", command=NewFile)
 filemenu.add_command(label="Open..", command=OpenFile)
+filemenu.add_command(label="Save..", command=OpenFile)
+filemenu.add_command(label="Save as..", command=SaveAs)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=root.quit)
 
 helpmenu = tk.Menu(menu)
-helpmenu.add_cascade(label="Help", menu=helpmenu)
+menu.add_cascade(label="Help", menu=helpmenu)
 helpmenu.add_command(label="About", command=About)
 
 
-T = tk.Text(root, height=3, width=30)
-T.pack()
+T = tk.Text(root, height=30, width=100)
+T.pack(expand = tk.YES, fill = tk.BOTH) ## expand - expand in y axis, fill - expand in x axis
 
 
 if __name__ == '__main__':
